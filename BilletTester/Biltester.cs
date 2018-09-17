9035402624af1e83ee316d1323daf4d70af01908
Søren Lykke Christensen <soer424c@edu.easj.dk>
2018-09-17 19:37:21 +0200

    Test commit

---
+++ BilletTester/Biltester.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BilletTester
+{
+    [TestClass]
+    public class Biltester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
